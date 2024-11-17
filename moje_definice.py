6d05492b14936874737906de043184080e5dffba
JosefKauc <kauc@email.cz>
2024-11-17 23:29:43 +0100

    Add files via upload

--- moje_definice.py
+++ moje_definice.py
@@ -70,6 +70,19 @@ def nacti_cislo_hrace():
     return cislo_hrace              # cislo hrace je string o čtyřech číslicich!
     
 def vyhodnot_cislo(cis_pocitac:str, cis_hrac:str) -> tuple:
+    """
+    Vstupem funkce jsou dva stringové parametry - čtyřmístné číslo počítače a hráče.
+
+    Vyhodnocení probíhá přes vytvořené slovníky, obsahující jako klíče jednotlivé číslice 
+    a jako hodnota daného klíče (tedy konkrétní číslice) je pozice číslice v zadaném stringu 
+
+    Pro každou číslici hráče ověří zda je ve slovníku počítače a zjistí její pozici v obou slovnících.
+    Porovnáním výsledku zjistí, zda pozice si jsou rovny, pak přičte hodnotu k bulls nebo si nejsou rovny, 
+    pak přičte cows.
+
+    Funkce Vrátí tuple o dvou hodnotách - na první pozici bude pocet trefených číslic na správnémmístě (bulls),
+    na druhém místě počet trefených číslic, ale na jiných pozicích (cows)
+    """
     
     slovnik_pocitac = dict()        # příprava slovníku pro znak a jeho pozici u čísla generovaného počítačem
     slovnik_hrac = dict()           # příprava slovníku pro znak a jeho pozici u čísla zadaného hráčem
@@ -88,9 +101,9 @@ def vyhodnot_cislo(cis_pocitac:str, cis_hrac:str) -> tuple:
     bulls = 0           # vynulovani počítadla pro každé vyhodnocení (bulls trefene císlo i pozice)
     cows = 0            # vynulovani počítadla pro každé vyhodnocení (cows trefene císlo ale ne pozice)
 
-    for znak_hrac in slovnik_hrac:
+    for znak_hrac in slovnik_hrac:         # pro každou číslici ze slovníku hráče         
         
-        if slovnik_pocitac.get(znak_hrac) in (0,1,2,3):
+        if slovnik_pocitac.get(znak_hrac) in (0,1,2,3):  # jestliže číslice hráče je ve slovniku pocitače, pak ...
 
             pozice_znaku_ze_slovniku_hrace = slovnik_hrac.get(znak_hrac)
             pozice_znaku_hrace_ze_slovniku_pocitace = slovnik_pocitac.get(znak_hrac)
@@ -138,6 +151,6 @@ if __name__ == "__main__":
     # print(generuj_cislo())
     # print(nacti_cislo_hrace())
     # print(vypis_vyhodnoceni(vyhodnot_cislo(generuj_cislo(), nacti_cislo_hrace())))
-    preved_cas(2750)  # test funkce s parametrem v sekundách
+    # preved_cas(2750)  # test funkce s parametrem v sekundách
     
     
